788ce32900585174e077f934439e78eb3c3a39bb
cgpalmer <cgpalmer91@gmail.com>
2020-03-15 09:33:52 +0000

    added a media query

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(".item1").click(function(){
     $(this).css("background-color", "red");
 });
+
+
+//You can add an icon with the addClass function. 
+//Set up the icon in css and then just have it added to your html <i>
