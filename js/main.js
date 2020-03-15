3c6963b28b1a0679cd996f5c6baf4e5193e5e179
cgpalmer <cgpalmer91@gmail.com>
2020-03-15 16:34:54 +0000

    added responsive padding and font size

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$(".grid-container div").click(function(){
-    $(this).css("background-color", "red");
+$(".emotions").click(function(){
+   $(this).fadeIn(3000).addClass("click-border");
+   $(this).append("<i class='fas fa-check'> </i>");
 });
 
 
+
+
+
+
+
+
 //You can add an icon with the addClass function. 
-//Set up the icon in css and then just have it added to your html <i>
+//Set up the icon in css and then just have it added to your html 
+/*<i id="icon" class="fas fa-check"></i> append this */
